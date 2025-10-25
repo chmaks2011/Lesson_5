c004ed70c0ae6b716e7d2fe1e7edf6f37132c7a5
chmaks2011 <chmaks2011@hmail.com>
2025-10-25 15:03:38 +0200

    electronicClock (local time only)

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function updateClock (time) {
     const now = new Date();
-    const hours = String(now.getUTCHours() + time);
-    const minutes = String(now.getUTCMinutes());
-    const seconds = String(now.getUTCSeconds());
+    const hours = String(now.getUTCHours() + 2).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 
-    console.log(`${hours}:${minutes}:${seconds}`);
+    //console.log(`${hours}:${minutes}:${seconds}`);
+
+    const selectTimeZona = document.getElementById('timeZona')
+    timeZona = parseInt(selectTimeZona.value, 10);
+    //selectTimeZona.textContent = timeZona;
+    selectTimeZona.addEventListener('change', updateClock(timeZona))
+
 }
 
 
@@ -19,13 +26,16 @@ let timeZona;
 timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
 let different_time_UTC = new Date().getTimezoneOffset() /60;
 
-console.log(timeZona);
-console.log(different_time_UTC);
+//console.log(timeZona);
+//console.log(different_time_UTC);
 
 
 
 
 
-updateClock(timeZona);
+//updateClock(timeZona);
+
 //updateClock(2);
 //setInterval(updateClock(2), 1000);
+//setInterval(updateClock(timeZona), 1000);
+setInterval(updateClock, 1000);
