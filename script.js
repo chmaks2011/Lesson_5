f3fb94e1571cb6746bb811effffffa9a7abe28b5
chmaks2011 <chmaks2011@hmail.com>
2025-10-25 15:20:11 +0200

    Clear files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function updateClock (time) {
+function updateClock() {
     const now = new Date();
     const hours = String(now.getUTCHours() + 2).padStart(2, '0');
     const minutes = String(now.getUTCMinutes()).padStart(2, '0');
@@ -6,36 +6,8 @@ function updateClock (time) {
 
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 
-    //console.log(`${hours}:${minutes}:${seconds}`);
-
-    const selectTimeZona = document.getElementById('timeZona')
-    timeZona = parseInt(selectTimeZona.value, 10);
-    //selectTimeZona.textContent = timeZona;
-    selectTimeZona.addEventListener('change', updateClock(timeZona))
 
 }
 
 
-
-//console.log(new Date());
-//console.log(new Date().getUTCHours() + 2);
-//console.log(new Date().getUTCMinutes());
-//console.log(new Date().getUTCSeconds());
-
-let timeZona;
-timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
-let different_time_UTC = new Date().getTimezoneOffset() /60;
-
-//console.log(timeZona);
-//console.log(different_time_UTC);
-
-
-
-
-
-//updateClock(timeZona);
-
-//updateClock(2);
-//setInterval(updateClock(2), 1000);
-//setInterval(updateClock(timeZona), 1000);
 setInterval(updateClock, 1000);
