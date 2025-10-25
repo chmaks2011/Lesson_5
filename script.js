3dc727c171f931579743f3ac3bae74336dad2ec4
chmaks2011 <chmaks2011@hmail.com>
2025-10-25 15:31:40 +0200

    Add select in index.html

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const clockElement = document.getElementById('clock');
+
 function updateClock (time) {
     const now = new Date();
     const hours = String(now.getUTCHours() + 2).padStart(2, '0');
     const minutes = String(now.getUTCMinutes()).padStart(2, '0');
     const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
-    // document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 
     //console.log(`${hours}:${minutes}:${seconds}`);
 
