fce59f8cfc990267325d103a1512cb90af9b4e5a
chmaks2011 <chmaks2011@hmail.com>
2025-11-01 14:48:36 +0100

    finished electrinic clockwith UTC time

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
 const clockElement = document.getElementById('clock');
+const selectTimeZona = document.getElementById('timeZona')
+const local_timeZone = document.getElementById('local_timeZone')
 
-function updateClock (time) {
+//function updateClock (time) {
+function updateClock () {
     const now = new Date();
-    const hours = String(now.getUTCHours() + 2).padStart(2, '0');
+
+    const timeZona = parseInt(selectTimeZona.value, 10)
+
+    const hours = String(now.getUTCHours() + timeZona).padStart(2, '0');
     const minutes = String(now.getUTCMinutes()).padStart(2, '0');
     const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
     clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 
-    //console.log(`${hours}:${minutes}:${seconds}`);
-
-    const selectTimeZona = document.getElementById('timeZona')
-    timeZona = parseInt(selectTimeZona.value, 10);
-    //selectTimeZona.textContent = timeZona;
-    selectTimeZona.addEventListener('change', updateClock(timeZona))
-
+    local_timeZone.textContent = Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
 
-
-//console.log(new Date());
-//console.log(new Date().getUTCHours() + 2);
-//console.log(new Date().getUTCMinutes());
-//console.log(new Date().getUTCSeconds());
-
-let timeZona;
-timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
-let different_time_UTC = new Date().getTimezoneOffset() /60;
-
-//console.log(timeZona);
-//console.log(different_time_UTC);
-
-
-
-
-
-//updateClock(timeZona);
-
-//updateClock(2);
-//setInterval(updateClock(2), 1000);
-//setInterval(updateClock(timeZona), 1000);
 setInterval(updateClock, 1000);
+selectTimeZona.addEventListener('change', updateClock)
